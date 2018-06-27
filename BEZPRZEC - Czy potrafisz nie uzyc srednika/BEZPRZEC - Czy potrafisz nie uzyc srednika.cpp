c76b721273081071c8c24d66e8e96a4ed2de03f7
Artur Andrulewicz <37901946+LionArt@users.noreply.github.com>
2018-06-27 21:02:20 +0200

    Add files via upload

---
+++ BEZPRZEC - Czy potrafisz nie uzyc srednika/BEZPRZEC - Czy potrafisz nie uzyc srednika.cpp
@@ -0,0 +1,23 @@
+/* Andrulewicz Artur
+BEZPRZEC - Czy potrafisz nie uzyc srednika
+2018-06-27
+*/
+
+#include <iostream>
+
+int main()
+{
+    if(int a=1)
+    {
+        if(int b=1)
+        {
+            if(std::cin>>a>>b)
+            {
+                if(std::cout<<a+b)
+                {
+
+                }
+            }
+        }
+    }
+}
